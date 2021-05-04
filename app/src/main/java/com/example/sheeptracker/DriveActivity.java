ed7a1b4027cbcde46173230b72e502bc315cbdb1
Tien Nguyen <tienguyen9@gmail.com>
2021-05-04 15:53:37 +0200

    testing repo

--- app/src/main/java/com/example/sheeptracker/DriveActivity.java
+++ app/src/main/java/com/example/sheeptracker/DriveActivity.java
@@ -25,6 +25,7 @@ public class DriveActivity extends Activity {
     TextView tv_email;
 
     @Override
+    //testing
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drive);
