d17c9db2503d2c567a39bd67d0cd175918c6d6bd
Tien Nguyen <tienguyen9@gmail.com>
2021-09-08 13:32:28 +0200

    moved location service

--- app/src/main/java/com/example/sheeptracker/MapActivity.java
+++ app/src/main/java/com/example/sheeptracker/MapActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import utility.Constants;
+import utility.LocationService;
 
 public abstract class MapActivity extends AppCompatActivity {
 
--- app/src/main/java/utility/LocationService.java
+++ app/src/main/java/utility/LocationService.java
@@ -1,4 +1,4 @@
-package com.example.sheeptracker;
+package utility;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -16,6 +16,7 @@ import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import com.example.sheeptracker.R;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
